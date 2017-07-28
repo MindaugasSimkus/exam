a3dd45a5f74e22f38838d6c8add457b11a78bdd2
MindaugasSimkus <shimkus.mindaugas@gmail.com>
2017-07-28 19:49:54 +0300

    task9 done

---
+++ task9.php
@@ -0,0 +1,41 @@
+<?php 
+
+class Person {
+	public $name;
+	public $surname;
+	public $age;
+
+	public function __construct($vardas, $pavarde, $amzius) {
+		$this->name = $vardas;
+		$this->surname = $pavarde;
+		$this->age = $amzius;
+	}
+
+	public function asmensDuomenys() {
+		echo $this->name . " " . $this->surname . " (" . $this->age . ") <br/>";
+
+	}
+
+	
+}
+
+class Staff extends Person {
+
+	public function asmensDuomenys() {
+		echo $this->surname . " " . $this->name . ", Darbuotojas <br/>";
+
+	}
+}
+
+class Client extends Person {}
+
+$Alytiskis = new Person('Valdas', 'Skikunas', 48);
+$Kelmiskis = new Staff('Mindaugas', 'Simkus', 27);
+$Uteniskis = new Client('Jonas', 'Valanciunas', 25);
+
+$Alytiskis->asmensDuomenys();
+$Kelmiskis->asmensDuomenys();
+$Uteniskis->asmensDuomenys();
+
+?>
+
