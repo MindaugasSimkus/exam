5523a251fab1d8bed39510962cbdd8e03a54d47d
MindaugasSimkus <shimkus.mindaugas@gmail.com>
2017-07-28 19:33:39 +0300

    task7 done

---
+++ task7.php
@@ -0,0 +1,10 @@
+<?php 
+
+	$tekstoMasyvas = ["as", "esu", "labai", "protingas", "galbut"];
+
+	foreach ($tekstoMasyvas as &$tekstas) {
+		$tekstas = strtoupper($tekstas);
+	}
+	print_r($tekstoMasyvas);
+?>
+
